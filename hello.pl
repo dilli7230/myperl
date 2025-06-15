c9f66f5cd29b073f6104b07a5dcd50c46e8e4b2e
dilli7230 <dilli7230@gmail.com>
2025-06-15 21:02:38 +0530

    Update hello.pl

--- hello.pl
+++ hello.pl
@@ -1,8 +1,5 @@
 #! /usr/bin/perl -w
-<<<<<<< HEAD
 print("This is my perl \n");
-=======
 print ("This is my perl\n");
 print ("This is 2ns line in programmimg\n");
 print ("This is 2ns line in programmimg\n");
->>>>>>> origin/main
